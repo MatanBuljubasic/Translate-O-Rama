6884c7ef11d22b29b5dfecb33d270f5e78f3eb63
Antonio_Veselcic <antonioveselcic0@gmail.com>
2023-02-02 16:28:40 +0100

    Add job listing

--- translate_o_rama/app/urls.py
+++ translate_o_rama/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "app"
 urlpatterns = [
     path('post/',  views.post,  name="post"),
+    path('job_listing/', views.job_listing, name="job_listing"),
 ]
--- translate_o_rama/app/views.py
+++ translate_o_rama/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse 
 from .forms import PostJobForm
+from .models import Job, STATUS_CHOICES, FIELD_CHOICES
 
 # Create your views here.
 
@@ -25,3 +26,13 @@ def post(request):
             return render(request, 'app/post_job.html', {'form': form})
     else:
         return HttpResponseRedirect(reverse('accounts:custom_login'))  
+
+def job_listing(request):
+    if request.user.is_authenticated:
+        jobs = Job.objects.filter(status=STATUS_CHOICES[0][0])
+        context = {
+            'jobs' : jobs,
+        }
+        return render(request, 'app/job_listing.html', context)
+    else:
+        return HttpResponseRedirect(reverse('accounts:custom_login'))
